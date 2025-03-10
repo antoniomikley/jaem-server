f22b89890b3d02c29ad3a6c8a50760e2eb8303b5
ItsJuzoSuzuya <schefner20@gmail.com>
2025-03-10 19:02:53 +0100

    New tests

--- jaem_user-discovery/tests/api_tests.rs
+++ jaem_user-discovery/tests/api_tests.rs
@@ -1,11 +1,15 @@
 use std::{
+    error::Error,
     fs,
     sync::{Arc, OnceLock},
 };
 
 use ctor::dtor;
+use http_body_util::{combinators::BoxBody, BodyExt};
+use hyper::{body::Bytes, Response};
 use hyper::{Method, Request, StatusCode};
 use jaem_user_discovery::user_data::UserStorage;
+use serde_json::Value;
 use tokio::sync::Mutex;
 
 const BASE_URI: &str = "http://127.0.0.1:8080";
@@ -45,7 +49,78 @@ async fn get_users_default_page_success() {
     )
     .await
     .unwrap();
+
     assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.collect().await.unwrap().to_bytes();
+    let binding = serde_json::from_slice::<Value>(&body).unwrap();
+    let json = binding.as_array().unwrap();
+    let size = json.len();
+
+    assert_eq!(size, 20);
+
+    let first_user = json[0].as_object().unwrap();
+    let username = first_user.get("username").unwrap().as_str().unwrap();
+    let description = first_user.get("description").unwrap().as_str().unwrap();
+    let profile_pic = first_user.get("profile_picture").unwrap().as_str().unwrap();
+
+    assert_eq!(username, "admin");
+    assert_eq!(description, "Administrator");
+    assert_eq!(profile_pic, "Im an Image\n");
+
+    let last_user = json[size - 1].as_object().unwrap();
+    let last_username = last_user.get("username").unwrap().as_str().unwrap();
+    let last_description = last_user.get("description").unwrap().as_str().unwrap();
+    let last_profile_pic = last_user.get("profile_picture").unwrap().as_str().unwrap();
+
+    assert_eq!(last_username, "User 17");
+    assert_eq!(last_description, "Additional User");
+    assert_eq!(last_profile_pic, "Hello Im profile picture 1017\n");
+}
+
+#[tokio::test]
+async fn get_users_from_10_to_14_success() {
+    let request = Request::builder()
+        .method(Method::GET)
+        .uri(format!("{}/users/2/5", BASE_URI))
+        .body("".to_string())
+        .unwrap();
+
+    let users = get_users();
+    let response = jaem_user_discovery::handle_connection::handle_connection(
+        request,
+        users.clone(),
+        "temp_users.json",
+    )
+    .await
+    .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.collect().await.unwrap().to_bytes();
+    let binding = serde_json::from_slice::<Value>(&body).unwrap();
+    let json = binding.as_array().unwrap();
+    let size = json.len();
+
+    assert_eq!(size, 5);
+
+    let first_user = json[0].as_object().unwrap();
+    let username = first_user.get("username").unwrap().as_str().unwrap();
+    let description = first_user.get("description").unwrap().as_str().unwrap();
+    let profile_pic = first_user.get("profile_picture").unwrap().as_str().unwrap();
+
+    assert_eq!(username, "User 8");
+    assert_eq!(description, "Additional User");
+    assert_eq!(profile_pic, "1008\n");
+
+    let last_user = json[4].as_object().unwrap();
+    let last_username = last_user.get("username").unwrap().as_str().unwrap();
+    let last_description = last_user.get("description").unwrap().as_str().unwrap();
+    let last_profile_pic = last_user.get("profile_picture").unwrap().as_str().unwrap();
+
+    assert_eq!(last_username, "User 12");
+    assert_eq!(last_description, "Additional User");
+    assert_eq!(last_profile_pic, "1012\n");
 }
 
 /// Test GET request to search by name
@@ -53,7 +128,7 @@ async fn get_users_default_page_success() {
 async fn filter_by_name_success() {
     let request = Request::builder()
         .method(Method::GET)
-        .uri(format!("{}/search_users/test", BASE_URI))
+        .uri(format!("{}/search_users/1", BASE_URI))
         .body("".to_string())
         .unwrap();
 
@@ -66,6 +141,13 @@ async fn filter_by_name_success() {
     .await
     .unwrap();
     assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.collect().await.unwrap().to_bytes();
+    let binding = serde_json::from_slice::<Value>(&body).unwrap();
+    let json = binding.as_array().unwrap();
+    let size = json.len();
+
+    assert_eq!(size, 14);
 }
 
 #[tokio::test]
